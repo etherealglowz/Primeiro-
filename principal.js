d94ab145867c7a79abc4082162544e8972362c81
etherealglowz <164558035+etherealglowz@users.noreply.github.com>
2024-03-22 16:22:51 -0300

    Add files via upload

---
+++ principal.js
@@ -0,0 +1,17 @@
+function temaEscuro()
+{
+    console.log('clicou no tema escuro')
+     document.getElementById('body').style.backgroundColor='black'
+     document.getElementById('paragrafo').style.color='white'
+     document.getElementById('h3').style.color='white'
+
+
+
+
+}
+function temaClaro(){
+    document.getElementById('body').style.backgroundColor='antiquewhite'
+    document.getElementById('paragrafo').style.color='black'
+    document.getElementById('h3').style.color='black'
+  
+}
